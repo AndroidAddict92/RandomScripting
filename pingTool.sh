127b5d83f3082bcad4c21ef5f889244b7015ec6b
Paul <androidaddict1992@gmail.com>
2022-12-19 16:24:21 -0600

    Added a very basic Bash ping tool.
    This script prompts the user for input on an IP address or Domain, the amount of packets to send, then creates a log file with the ping information.

---
+++ pingTool.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# Created script to freshen up on Bash
+# This script is for running a basic ping command based on user input
+# Be sure you run chmod +x pingTool.sh to set the executable permission
+
+# Prompt user for input
+echo "Please enter your IP or Domain to ping: "
+
+# Read the users input
+read cmd
+
+# Ask how many packets to send
+echo "How many packets would you like to send: "
+
+# Read packet input
+read packet
+
+# Execute users input
+if [[ "$cmd -eq $cmd" ]]
+  then
+  ping -c $packet $cmd > ping_log.txt
+  echo "Ping Complete! Press any key to view the log! "
+  read key
+  if [[ "$key -eq $key" ]]
+    then
+	cat ping_log.txt
+  fi
+fi
+
